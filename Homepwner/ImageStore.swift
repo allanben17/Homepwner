362c2052f3854c725d61cdfcc3e4cf0e57b6c5ac
Allan Ben <allanben0417@gmail.com>
2016-07-23 18:46:41 -0400

    added persistence for Items and itemImages by adopting NSCoding for Item, saving image as png in DocumentDir

--- Homepwner/ImageStore.swift
+++ Homepwner/ImageStore.swift
@@ -14,14 +14,48 @@ class ImageStore {
 
 	func setImage(image: UIImage, forKey key: String) {
 		cache.setObject(image, forKey: key)
+
+		// Create full URL for image
+		let imageURL = self.imageURLForKey(key)
+
+		// Turn iamge into JPEG data
+		if let data = UIImagePNGRepresentation(image) {
+			// Write it to full URL
+			data.writeToURL(imageURL, atomically: true)
+		}
 	}
 
 	func imageForKey(key: String) -> UIImage? {
-		return cache.objectForKey(key) as? UIImage
+
+		if let existingImage = self.cache.objectForKey(key) as? UIImage {
+			return existingImage
+		}
+
+		let imageURL = self.imageURLForKey(key)
+		guard let imageFromDisk = UIImage(contentsOfFile: imageURL.path!) else {
+			return nil
+		}
+
+		self.cache.setObject(imageFromDisk, forKey: key)
+		return imageFromDisk
 	}
 
 	func deleteImageForKey(key: String) {
 		cache.removeObjectForKey(key)
+
+		let imageURL = self.imageURLForKey(key)
+		do {
+			try NSFileManager.defaultManager().removeItemAtURL(imageURL)
+		} catch let deleteError {
+			print("Error removing the image from disk: \(deleteError)")
+		}
 	}
 
+	func imageURLForKey(key: String) -> NSURL {
+
+		let documentsDirectories = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+		let documentDirectory = documentsDirectories.first!
+
+		return documentDirectory.URLByAppendingPathComponent(key)
+	}
 }
